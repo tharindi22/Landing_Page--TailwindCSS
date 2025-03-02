f13132b872d5d8a8f6b147784e78b6d21ff76116
tharindi22 <ttharindiattanayaka@gmail.com>
2025-03-02 21:53:52 +0530

    Added header to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.html", "./**/*.js"],
+export default {
+  content: ["*.{html,js}"],
   theme: {
     container:{
       padding: {
