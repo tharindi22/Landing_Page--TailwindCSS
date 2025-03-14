354a28930c186413a494110d7831535fe8b2d446
tharindi22 <ttharindiattanayaka@gmail.com>
2025-03-14 22:38:38 +0530

    java sript for feedback

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ const navMenu = document.querySelector('#nav_menu');
 navTriggerBtn.addEventListener('click', ()=>{
     navMenu.classList.toggle('nav-is-open');
 });
+
+//swiper
+const swiper = new Swiper('.swiper',{
+    loop:true,
+    pagination:{
+        el:'.swiper-pagination'
+    }
+})
